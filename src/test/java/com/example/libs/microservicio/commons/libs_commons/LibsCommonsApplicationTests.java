f3ab64b817b4bcd60b2be2feb217bff8060c2daa
MarioQL <mquiros@epromcr.com>
2025-01-09 11:45:55 -0600

    Creacion de proyecto con commons de microservicios

---
+++ src/test/java/com/example/libs/microservicio/commons/libs_commons/LibsCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libs.microservicio.commons.libs_commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibsCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
